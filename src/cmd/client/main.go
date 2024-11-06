160cd5c9c08d883cb309f47ed89271b71bfd58b3
Calamarp <calamarp@student.21-school.ru>
2024-11-06 15:59:25 +0300

    add log for client and anomaly detected

--- src/cmd/client/main.go
+++ src/cmd/client/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"flag"
 	"log"
+	"math"
 
 	pb "alien-contact-detector/internal/pkg" // убедитесь, что путь правильный
 
@@ -10,8 +12,53 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
+const (
+	defaultK       = 1.5
+	defaultAddress = "localhost:3333"
+)
+
+func parseFlags() (k *float64) {
+	k = flag.Float64("k", defaultK, "Anomaly detection coefficient")
+	flag.Parse()
+	return
+}
+
+func analyzeFrequencies(stream pb.TransmitterService_GenerateFrequencyClient, k float64) {
+	var (
+		sum    float64 // Сумма всех частот для вычисления среднего значения
+		sumSq  float64 // Сумма квадратов частот для вычисления дисперсии
+		count  int     // Счетчик полученных сообщений
+		mean   float64 // Среднее значение частот
+		stdDev float64 // Стандартное отклонение частот
+	)
+	for {
+		data, err := stream.Recv()
+		if err != nil {
+			log.Printf("Error receiving data: %v", err)
+			break
+		}
+		count++
+		frequency := data.GetFrequency()
+		sum += frequency
+		sumSq += frequency * frequency
+		if count > 0 {
+			mean = sum / float64(count)
+			variance := (sumSq / float64(count)) - (mean * mean) // дисперсии
+			if variance < 0 {
+				variance = 0
+			}
+			stdDev = math.Sqrt(variance)
+		}
+		log.Printf("Received frequency: %f, Mean: %f, StdDev: %f", frequency, mean, stdDev)
+		if math.Abs(frequency-mean) > k*stdDev {
+			log.Printf("Anomaly detected! Frequency: %f, Mean: %f, StdDev: %f", frequency, mean, stdDev)
+		}
+	}
+}
+
 func main() {
-	conn, err := grpc.NewClient("localhost:3333", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	k := parseFlags()
+	conn, err := grpc.NewClient(defaultAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
@@ -20,19 +67,12 @@ func main() {
 	client := pb.NewTransmitterServiceClient(conn)
 
 	// Это хорошая практика
-	numValues := int32(1) // количество запросов
+	numValues := int32(10) // количество запросов
 
 	stream, err := client.GenerateFrequency(context.Background(), &pb.FrequencyRequest{NumValues: numValues})
 	if err != nil {
 		log.Fatalf("Error calling GenerateFrequency: %v", err)
 	}
 
-	for {
-		data, err := stream.Recv()
-		if err != nil {
-			log.Printf("Error receiving data: %v", err)
-			break
-		}
-		log.Printf("Received data: session_id=%s, frequency=%.2f, timestamp=%d", data.SessionId, data.Frequency, data.Timestamp)
-	}
+	analyzeFrequencies(stream, *k)
 }
